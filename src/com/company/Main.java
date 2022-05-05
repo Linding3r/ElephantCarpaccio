d712eebaae17cab0b23a7cf08efaa6f03d4fed27
Linding3r <thomasjuullindinger@yahoo.dk>
2022-05-05 11:04:16 +0200

    stateCodes added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     private int itemAmount;
     private double itemprice;
+    private double taxRate;
+    private double procentage;
+    private String stateCode;
 
     public void inputItemAmount(){
         System.out.println("Please type the amount of items:");
@@ -16,7 +19,33 @@ public class Main {
     public void itemPrice(){
         System.out.println("Please input the price of the item");
         itemprice = sc.nextDouble();
+        sc.nextLine();
         System.out.println("\n\n\nThe price of the item is " + itemprice + "$");
+    }
+
+    public double stateCodes(){
+        boolean run = true;
+        while(run) {
+            run = false;
+            System.out.println("Please enter the state code:");
+            stateCode = sc.nextLine().toUpperCase();
+            switch (stateCode) {
+                case "UT" -> procentage = 6.85;
+                case "NV" -> procentage = 8.00;
+                case "TX" -> procentage = 6.25;
+                case "AL" -> procentage = 4.00;
+                case "CA" -> procentage = 8.25;
+                default -> {
+                    System.out.println(stateCode + " is not in the list of state codes");
+                    run = true;
+                }
+            }
+        }return procentage;
+    }
+
+    public void inputStateCode(){
+        taxRate = stateCodes();
+        System.out.println("\n\n\nYou have entered " + stateCode + " which has a " + taxRate + "% tax rate.");
     }
 
 
@@ -25,6 +54,7 @@ public class Main {
     public void run(){
         inputItemAmount();
         itemPrice();
+        inputStateCode();
     }
 
     public static void main(String[] args) {
