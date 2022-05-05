23658cb7bf68886ed8533c0221b57e978aa6633d
Linding3r <thomasjuullindinger@yahoo.dk>
2022-05-05 10:39:03 +0200

    inputItemAmount

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner sc = new Scanner(System.in);
+    private int itemAmount;
+
+    public void inputItemAmount(){
+        System.out.println("Please type the amount of items:");
+        itemAmount = sc.nextInt();
+        System.out.println("\n\n\nYou have " + itemAmount + " item(s) to play with!");
+    }
+
+    public void run(){
+        inputItemAmount();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hi");
+
+        Main prg = new Main();
+        prg.run();
     }
 }
