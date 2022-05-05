60e552491d4337dd6f22847fee6705ac6cd7ecb9
Linding3r <thomasjuullindinger@yahoo.dk>
2022-05-05 10:44:23 +0200

    inputItemPrice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     Scanner sc = new Scanner(System.in);
     private int itemAmount;
+    private double itemprice;
 
     public void inputItemAmount(){
         System.out.println("Please type the amount of items:");
@@ -12,8 +13,18 @@ public class Main {
         System.out.println("\n\n\nYou have " + itemAmount + " item(s) to play with!");
     }
 
+    public void itemPrice(){
+        System.out.println("Please input the price of the item");
+        itemprice = sc.nextDouble();
+        System.out.println("\n\n\nThe price of the item is " + itemprice + "$");
+    }
+
+
+
+
     public void run(){
         inputItemAmount();
+        itemPrice();
     }
 
     public static void main(String[] args) {
